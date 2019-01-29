169af96a95f3f35b65ddf13e67441a987c6b6bb0
Николай <n.bobrovnik@aic.ru>
2019-01-30 01:00:40 +0300

    123

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
-import { Route, Switch, withRouter } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import HomePage from './HomePage/HomePage'
-import Page404 from 'pages/Page404/Page404'
+import HomePage from "./HomePage/HomePage";
+import Page404 from "pages/Page404/Page404";
 
-export const PAGE_HOME = '/'
+export const PAGE_HOME = "/";
 
 @withRouter
 export default class Routes extends Component {
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     if (this.props.location.pathname !== nextProps.location.pathname) {
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
     }
   }
 
-  render () {
+  render() {
     return (
       <Switch>
         {/* главная */}
         <Route {...{ exact: true, path: PAGE_HOME, component: HomePage }} />
-        <Route {...{ path: '*', component: Page404 }} />
+        <Route {...{ path: "*", component: Page404 }} />
       </Switch>
-    )
+    );
   }
 }
 
 Routes.propTypes = {
   location: PropTypes.object
-}
+};
