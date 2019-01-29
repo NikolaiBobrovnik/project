be525485c149cfb38da0e5f904792f20fd795c30
Николай <n.bobrovnik@aic.ru>
2019-01-30 00:59:20 +0300

    commit

--- src/pages/DetailPage/DetailPage.js
+++ src/pages/DetailPage/DetailPage.js
@@ -1,40 +1,46 @@
-import React, { PureComponent } from 'react'
-import { RemoteDataProvider } from 'remote-data-provider'
-import PropTypes from 'prop-types'
-import { withRouter } from 'react-router-dom'
-import { isBrowser } from 'utils'
+import React, { PureComponent } from "react";
+import { RemoteDataProvider } from "remote-data-provider";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 
-import css from './detailPage.scss'
-import classNames from 'classnames'
+import { isBrowser } from "utils";
 
-import BrandHeroBanner from 'kalashnikov-framework/lib/components/BrandHeroBanner'
-import BrandLinkLayout from 'kalashnikov-framework/lib/components/BrandLink/BrandLinkLayout'
-import ActivitiesCardsList from 'kalashnikov-framework/lib/components/ActiivitiesCardsList'
-import FormSubscribe from 'containers/FormSubscribe/FormSubscribe'
-import News from 'containers/News/News'
-import MainAsideSection from 'kalashnikov-framework/lib/components/MainAsideSection'
-import PhotoSlider from 'kalashnikov-framework/lib/components/PhotoSlider'
-import InzhiniringContent from 'containers/InzhiniringContent/InzhiniringContent'
-import DetailVideo from 'containers/DetailVideo/DetailVideo'
-import DocumentationLinkLayout from 'kalashnikov-framework/lib/components/DocumentationLinkLayout'
-import TrainingCardLayout from 'kalashnikov-framework/lib/components/TrainingCardLayout'
+import css from "./detailPage.scss";
+import classNames from "classnames";
+
+import BrandHeroBanner from "kalashnikov-framework/lib/components/BrandHeroBanner";
+import BrandLinkLayout from "kalashnikov-framework/lib/components/BrandLink/BrandLinkLayout";
+import ActivitiesCardsList from "kalashnikov-framework/lib/components/ActiivitiesCardsList";
+import FormSubscribe from "containers/FormSubscribe/FormSubscribe";
+import News from "containers/News/News";
+import MainAsideSection from "kalashnikov-framework/lib/components/MainAsideSection";
+import PhotoSlider from "kalashnikov-framework/lib/components/PhotoSlider";
+import InzhiniringContent from "containers/InzhiniringContent/InzhiniringContent";
+import DetailVideo from "containers/DetailVideo/DetailVideo";
+import DocumentationLinkLayout from "kalashnikov-framework/lib/components/DocumentationLinkLayout";
+import TrainingCardLayout from "kalashnikov-framework/lib/components/TrainingCardLayout";
 // import ExamplesProductsList from 'kalashnikov-framework/lib/components/ExamplesProductsList'
 // import Information from 'containers/Information/Information'
 
 @withRouter
 class DetailPage extends PureComponent {
-  render () {
-    const { isMobile, match: { params: { code } } } = this.props
+  render() {
+    const {
+      isMobile,
+      match: {
+        params: { code }
+      }
+    } = this.props;
     return (
       <RemoteDataProvider
         {...{
           request: {
-            url: '/activity/detail.php',
+            url: "/activity/detail.php",
             params: {
               code
             }
           },
-          reducerKey: 'DetailPage'
+          reducerKey: "DetailPage"
         }}
       >
         {({
@@ -68,9 +74,9 @@ class DetailPage extends PureComponent {
                   breadcrumbs,
                   isBrowser,
                   isMobile,
-                  titleSize: isMobile ? 'm' : 'l',
+                  titleSize: isMobile ? "m" : "l",
                   parallax: !isMobile,
-                  blockPaddingBottom: isMobile ? 's' : 'none',
+                  blockPaddingBottom: isMobile ? "s" : "none",
                   ...banner
                 }}
               />
@@ -86,34 +92,36 @@ class DetailPage extends PureComponent {
                     />
                   </div>
                 )}
-                {documentation &&
-                <DocumentationLinkLayout
-                  {...{
-                    data: documentation,
-                    className: css.documentation,
-                    isMobile
-                  }}
-                />
-                }
-                {training &&
-                <MainAsideSection
-                  {...{
-                    isMobile,
-                    separator: true,
-                    reverse: true,
-                    asideWidth: 3,
-                    title: training.title,
-                    description: training.description,
-                    className: css.training,
-                    asideChildren: ''
-                  }}
-                >
-                  <TrainingCardLayout {...{ isMobile, training: training.items }} />
-                </MainAsideSection>
-                }
-                {isBrand && items &&
-                <InzhiniringContent {...{items, isMobile}} />
-                }
+                {documentation && (
+                  <DocumentationLinkLayout
+                    {...{
+                      data: documentation,
+                      className: css.documentation,
+                      isMobile
+                    }}
+                  />
+                )}
+                {training && (
+                  <MainAsideSection
+                    {...{
+                      isMobile,
+                      separator: true,
+                      reverse: true,
+                      asideWidth: 3,
+                      title: training.title,
+                      description: training.description,
+                      className: css.training,
+                      asideChildren: ""
+                    }}
+                  >
+                    <TrainingCardLayout
+                      {...{ isMobile, training: training.items }}
+                    />
+                  </MainAsideSection>
+                )}
+                {isBrand && items && (
+                  <InzhiniringContent {...{ items, isMobile }} />
+                )}
                 {moments && (
                   <MainAsideSection
                     {...{
@@ -163,17 +171,17 @@ class DetailPage extends PureComponent {
                 </MainAsideSection>
               )} */}
               <div {...{ className: css.container }}>
-                {slider &&
-                <PhotoSlider
-                  {...{
-                    arrow: 'left',
-                    isMobile,
-                    className: css.slider_one,
-                    items: slider.items,
-                    textPosition: false
-                  }}
-                />
-                }
+                {slider && (
+                  <PhotoSlider
+                    {...{
+                      arrow: "left",
+                      isMobile,
+                      className: css.slider_one,
+                      items: slider.items,
+                      textPosition: false
+                    }}
+                  />
+                )}
               </div>
               <News {...{ isMobile, className: css.news_container }} />
               <FormSubscribe {...{ isMobile, className: css.subscribe }} />
@@ -187,10 +195,10 @@ class DetailPage extends PureComponent {
                 }}
               />
             </div>
-          )
+          );
         }}
       </RemoteDataProvider>
-    )
+    );
   }
 }
 
@@ -198,6 +206,6 @@ DetailPage.propTypes = {
   response: PropTypes.object,
   match: PropTypes.object,
   isMobile: PropTypes.bool
-}
+};
 
-export default DetailPage
+export default DetailPage;
--- src/pages/PressCenterPage/PressCenterPage.js
+++ src/pages/PressCenterPage/PressCenterPage.js
@@ -1,69 +1,79 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import { withRouter } from 'react-router-dom'
-import classNames from 'classnames'
-import { connect } from 'react-redux'
-import toJS from 'HOC/toJS'
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 
-import css from './pressCenterPage.scss'
+import { withRouter } from "react-router-dom";
+import classNames from "classnames";
+import { connect } from "react-redux";
+import toJS from "HOC/toJS";
 
-import FormSubscribe from 'containers/FormSubscribe/FormSubscribe'
-import PressCenterNews from 'containers/PressCenterNews/PressCenterNews'
-import FormContacts from 'containers/FormContacts/FormContacts'
-import HeroBanner from 'containers/HeroBanner/HeroBanner'
-import NavBar from 'containers/NavBar/NavBar'
+import css from "./pressCenterPage.scss";
+
+import FormSubscribe from "containers/FormSubscribe/FormSubscribe";
+import PressCenterNews from "containers/PressCenterNews/PressCenterNews";
+import FormContacts from "containers/FormContacts/FormContacts";
+import HeroBanner from "containers/HeroBanner/HeroBanner";
+import NavBar from "containers/NavBar/NavBar";
 // import Information from 'containers/Information/Information'
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
-    navBar: state.getIn(['remoteData', 'PressCenterBanner', 'response', 'navBar'])
-  }
+    navBar: state.getIn([
+      "remoteData",
+      "PressCenterBanner",
+      "response",
+      "navBar"
+    ])
+  };
 }
 
 @withRouter
 @connect(mapStateToProps)
 @toJS
 class PressCenterPage extends PureComponent {
-  componentWillMount () {
+  componentWillMount() {
     // если открываем страницу, а params пустой, то открываем первый таб
     if (!this.props.match.params.iCode) {
-      this.props.history.push('/press-center/news/')
+      this.props.history.push("/press-center/news/");
     }
   }
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     // если изменяется страница, а params пустой, то открываем первый таб
     if (!nextProps.match.params.iCode) {
-      this.props.history.push('/press-center/news/')
+      this.props.history.push("/press-center/news/");
     }
   }
 
-  render () {
+  render() {
     const {
       isMobile,
-      match: { params: { iCode } },
+      match: {
+        params: { iCode }
+      },
       navBar
-    } = this.props
+    } = this.props;
 
     return (
-      <div {...{
-        className: classNames(
-          css.wrapper,
-          isMobile ? css.mobile : css.desktop
-        )
-      }}>
+      <div
+        {...{
+          className: classNames(
+            css.wrapper,
+            isMobile ? css.mobile : css.desktop
+          )
+        }}
+      >
         <HeroBanner
           {...{
             isMobile,
-            url: '/press-center/tabs.php',
-            reducerKey: 'PressCenterBanner',
+            url: "/press-center/tabs.php",
+            reducerKey: "PressCenterBanner",
             className: isMobile ? css.banner_mobile : css.banner_desktop,
             bannerProps: {
-              parallaxHeight: '70rem',
-              blockPaddingBottom: isMobile ? 'm' : 'l'
+              parallaxHeight: "70rem",
+              blockPaddingBottom: isMobile ? "m" : "l"
             },
             breadcrumbsFromTabs: [
-              { link: '/press-center/', title: 'Пресс-центр' }
+              { link: "/press-center/", title: "Пресс-центр" }
             ],
             code: iCode
           }}
@@ -75,38 +85,38 @@ class PressCenterPage extends PureComponent {
             navBar
           }}
         />
-        {(!iCode || iCode === 'news') && (
+        {(!iCode || iCode === "news") && (
           <PressCenterNews {...{ isMobile, className: css.news }} />
         )}
-        {iCode === 'contacts' && (
+        {iCode === "contacts" && (
           <FormContacts
             {...{
               className: css.contacts,
-              requestUrl: '/press-center/contacts.php',
-              reducerKey: 'pressCenterContacts',
-              form: 'pressCenterContacts',
+              requestUrl: "/press-center/contacts.php",
+              reducerKey: "pressCenterContacts",
+              form: "pressCenterContacts",
               isMobile,
               withForm: false,
-              reCaptchaAction: 'group_mail_press-center'
+              reCaptchaAction: "group_mail_press-center"
             }}
           />
         )}
         <FormSubscribe {...{ isMobile, className: css.subscribe }} />
         {/* <Information {...{ isMobile }} /> */}
       </div>
-    )
+    );
   }
 }
 
 PressCenterPage.defaultProps = {
   isMobile: false
-}
+};
 
 PressCenterPage.propTypes = {
   history: PropTypes.object,
   match: PropTypes.object,
   isMobile: PropTypes.bool,
   navBar: PropTypes.object
-}
+};
 
-export default PressCenterPage
+export default PressCenterPage;
